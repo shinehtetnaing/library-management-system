1a4cbc2bbe6bc59fb4fe5aac01d16c8fb416d8a7
shinehtetnaing <shinehtetnaing1998@gmail.com>
2025-05-07 14:08:02 +0700

    setup drizzle

--- lib/config.ts
+++ lib/config.ts
@@ -6,6 +6,7 @@ const config = {
       publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
       privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
     },
+    databaseUrl: process.env.DATABASE_URL!,
   },
 };
 
