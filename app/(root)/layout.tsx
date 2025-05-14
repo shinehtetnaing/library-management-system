025dc1fc69536d833d9ba85e08142d686cc39d4f
shinehtetnaing <shinehtetnaing1998@gmail.com>
2025-05-14 13:20:45 +0700

    implement user last activity tracking

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,10 @@
 import { auth } from "@/auth";
 import Header from "@/components/Header";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
+import { after } from "next/server";
 import { ReactNode } from "react";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
@@ -8,10 +12,30 @@ const Layout = async ({ children }: { children: ReactNode }) => {
 
   if (!session) redirect("/sign-in");
 
+  after(async () => {
+    if (!session.user?.id) return;
+
+    // get the user and see if the last activity date is today
+    const user = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session.user.id))
+      .limit(1);
+
+    if (user[0].lastActivityDate === new Date().toISOString().slice(0, 10))
+      return;
+
+    await db
+      .update(users)
+      .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
+      .where(eq(users.id, session.user.id));
+  });
+
   return (
     <main className="root-container">
       <div className="mx-auto max-w-7xl">
         <Header session={session} />
+
         <div className="mt-20 pb-20">{children}</div>
       </div>
     </main>
